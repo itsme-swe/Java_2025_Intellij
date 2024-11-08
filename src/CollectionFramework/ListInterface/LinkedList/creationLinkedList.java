11b340d6d62ab9ceee5d5bad58844dc4b35bde79
itsme-swe <me.harshmehra07@gmail.com>
2024-11-08 09:21:38 +0530

    Update

---
+++ src/CollectionFramework/ListInterface/LinkedList/creationLinkedList.java
@@ -0,0 +1,19 @@
+package CollectionFramework.ListInterface.LinkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class creationLinkedList {
+    public static void main(String[] args) {
+
+        List <Integer> ll = new LinkedList<>();
+
+        ll.add(10);
+        ll.add(5);
+        ll.add(15);
+        ll.add(20);
+        ll.add(12);
+
+        System.out.println(ll);
+    }
+}
